e7963a1946b6aedd6f3c60c3c21401ec2d3a3225
Dror Maman <drormaman@gmail.com>
2020-10-05 16:38:42 +0300

    Update recoil.test.js

--- recoil.test.js
+++ recoil.test.js
@@ -2,11 +2,11 @@ const puppeteer = require("puppeteer");
 
 let page;
 let browser;
-jest.setTimeout(8000);
+jest.setTimeout(30000);
 describe("Can add background color functionality", () => {
 	beforeAll(async () => {
 		//open a chromium browser
-		browser = await puppeteer.launch({ headless: true });
+		browser = await puppeteer.launch({ headless: false });
 		//open a new page within that browser
 		page = await browser.newPage();
 		//go to our app
@@ -93,7 +93,7 @@ function rgbToHex(rgb) {
 describe("Can Add a shape from Menu", () => {
 	beforeAll(async () => {
 		//open a chromium browser
-		browser = await puppeteer.launch({ headless: true });
+		browser = await puppeteer.launch({ headless: false });
 		//open a new page within that browser
 		page = await browser.newPage();
 		await page.goto("http://localhost:3000/", { waitUntil: "networkidle0" });
@@ -101,39 +101,6 @@ describe("Can Add a shape from Menu", () => {
 	afterAll(async () => {
 		//close the chromium after each test
 		await browser.close();
-	});
-
-	test("can create a rectangle both in the sidebar and the canvas", async () => {
-		// click on the width input and press arrow down to change the menu width value to 74px
-		await page.waitForSelector("#root #widthInput");
-		await page.click("#root #widthInput");
-		await page.keyboard.press("ArrowDown");
-		//click the add Shape button
-		await page.waitForSelector(
-			"#root > .app > .sidebar > .sidebar__menu > button:nth-child(7)"
-		);
-		await page.click(
-			"#root > .app > .sidebar > .sidebar__menu > button:nth-child(7)"
-		);
-		// get the element in the sideBar list
-		const listItem = await page.$(".sidebar__row");
-		// get the element at the canvas
-		const canvasItem = await page.$(".rect");
-		page.waitForSelector(".dsdscdscscddsnkd");
-		// get the width of both items
-		const widthInListItem = await page.$eval(
-			"#shapeWidthInput",
-			element => element.value
-		);
-		const widthInCanvasItem = await page.$eval(
-			".rect",
-			element => element.style.width
-		);
-
-		// ASSERTIONS
-		expect(listItem).toBeDefined();
-		expect(canvasItem).toBeDefined();
-		expect(widthInListItem + "px").toBe(widthInCanvasItem);
 	});
 
 	test("can do it without the app, and the sideBar component's rendering", async () => {
@@ -159,7 +126,7 @@ describe("Can Add a shape from Menu", () => {
 		);
 		const widthInCanvasItem = await page.$eval(
 			".rect",
-			element => element.style.width
+			element => getComputedStyle(element).width
 		);
 
 		// ASSERTIONS
@@ -189,14 +156,14 @@ describe("Can Add a shape from Menu", () => {
 });
 
 describe("Edit rectangle", () => {
-	beforeAll(async () => {
+	beforeEach(async () => {
 		//open a chromium browser
-		browser = await puppeteer.launch({ headless: true });
+		browser = await puppeteer.launch({ headless: false, slowMo: 500 });
 		//open a new page within that browser
 		page = await browser.newPage();
 		await page.goto("http://localhost:3000/", { waitUntil: "networkidle0" });
 	});
-	afterAll(async () => {
+	afterEach(async () => {
 		//close the chromium after each test
 		await browser.close();
 	});
@@ -230,7 +197,10 @@ describe("Edit rectangle", () => {
 		const listItem = await page.$(".sidebar__row");
 		const canvasItem = await page.$(".rect");
 		const widthInListItem = await page.$eval("#shapeWidthInput", e => e.value);
-		const widthInCanvasItem = await page.$eval(".rect", e => e.style.width);
+		const widthInCanvasItem = await page.$eval(
+			".rect",
+			element => getComputedStyle(element).width
+		);
 		// ASSERTIONS
 		expect(listItem).toBeDefined();
 		expect(canvasItem).toBeDefined();
